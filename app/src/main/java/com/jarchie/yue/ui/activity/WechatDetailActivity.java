dda9a0dccf88b2cc37ac8844bdd2ba070feebc32
jarchie520 <jarchie520@163.com>
2018-02-05 16:16:47 +0800

    优化新闻详情WebView页面加载的状态处理

--- app/src/main/java/com/jarchie/yue/ui/activity/WechatDetailActivity.java
+++ app/src/main/java/com/jarchie/yue/ui/activity/WechatDetailActivity.java
@@ -3,26 +3,37 @@ package com.jarchie.yue.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import com.jarchie.common.utils.ActivityManager;
+import com.jarchie.common.widget.LoadingTip;
+import com.jarchie.common.widget.RefreshInitView;
 import com.jarchie.yue.R;
-import com.jarchie.yue.constant.Constant;
 import com.jarchie.yue.ui.widget.CustomToolBar;
-import com.jarchie.yue.utils.CommonUtils;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.header.ClassicsHeader;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class WechatDetailActivity extends AppCompatActivity {
+public class WechatDetailActivity extends AppCompatActivity implements OnRefreshListener {
 
     @Bind(R.id.mWebView)
     WebView mWebView;
     @Bind(R.id.mToolbar)
     CustomToolBar mToolbar;
+    @Bind(R.id.mHeader)
+    ClassicsHeader mHeader;
+    @Bind(R.id.mLoadedTip)
+    LoadingTip mLoadedTip;
+    @Bind(R.id.mRefreshLayout)
+    SmartRefreshLayout mRefreshLayout;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,18 +46,21 @@ public class WechatDetailActivity extends AppCompatActivity {
     }
 
     private void initListener() {
+        mRefreshLayout.setOnRefreshListener(this);
+        RefreshInitView.initView(mRefreshLayout);
         mToolbar.setLeftBtnListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                CommonUtils.backPreviousPage(WechatDetailActivity.this, Constant.BACK_PREVIOUS_ACTIVITY, KeyEvent.KEYCODE_BACK, mWebView);
+                finish();
             }
         });
     }
 
     private void initData() {
         Intent intent = getIntent();
-        final String url = intent.getStringExtra("url");
+        url = intent.getStringExtra("url");
         mToolbar.setTitle("新闻详情");
+        RefreshInitView.initDataView(mRefreshLayout, this);
         mWebView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
         mWebView.getSettings().setDomStorageEnabled(true);
         //加载网页的处理逻辑
@@ -56,34 +70,34 @@ public class WechatDetailActivity extends AppCompatActivity {
         mWebView.getSettings().setBuiltInZoomControls(true);
         //接口回调
         mWebView.setWebChromeClient(new WebViewClient());
+        loadingPage(url);
+    }
+
+    @Override
+    public void onRefresh(RefreshLayout refreshlayout) {
+        loadingPage(url);
+        mRefreshLayout.finishRefresh();
+    }
+
+    //加载显示
+    private void loadingPage(final String mUrl) {
+        showLoading();
         //加载网页
-        mWebView.loadUrl(url);
+        mWebView.loadUrl(mUrl);
         //本地显示
         mWebView.setWebViewClient(new android.webkit.WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                if (url.contains("http://")) {
-                    view.loadUrl(url);
-                }
-                //接收这个事件
-                return false;
+                view.loadUrl(mUrl);
+                return true;
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                showErrorTip();
             }
         });
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (mWebView.canGoBack()) {
-            mWebView.goBack();
-        } else {
-            finish();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        ActivityManager.getInstance().removeActivity(this);
     }
 
     //自定义WebviewClient
@@ -94,9 +108,38 @@ public class WechatDetailActivity extends AppCompatActivity {
             super.onProgressChanged(view, newProgress);
             //处理进度,加载完成时隐藏进度条
             if (newProgress >= 100) {
-
+                stopLoading();
             }
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityManager.getInstance().removeActivity(this);
+    }
+
+    public void showLoading() {
+        mWebView.setVisibility(View.GONE);
+        mHeader.setVisibility(View.GONE);
+        mLoadedTip.setLoadingTip(LoadingTip.LoadStatus.loading);
+    }
+
+    public void stopLoading() {
+        mLoadedTip.setLoadingTip(LoadingTip.LoadStatus.finish);
+        mWebView.setVisibility(View.VISIBLE);
+        mHeader.setVisibility(View.VISIBLE);
+    }
+
+    public void showErrorTip() {
+        mWebView.setVisibility(View.GONE);
+        mHeader.setVisibility(View.GONE);
+        mLoadedTip.setLoadingTip(LoadingTip.LoadStatus.error);
     }
 
 }
